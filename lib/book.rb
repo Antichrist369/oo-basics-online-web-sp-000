17f5a623c649fd3a059ae7c57ec28e39bf280ac1
Anthony Molina <antonio-molina@att.net>
2019-06-23 04:59:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,34 +1,12 @@
 class Book
+  attr_accessor :athor, :page_count, :genre 
+  
   def initialize(title) 
     @title = title 
   end 
   
   def title 
     @title
-  end 
-  
-  def author=(author) #setter method 
-    @author = author 
-  end 
-  
-  def author #getter method
-    @author
-  end 
-  
-  def page_count=(num)
-    @page_count=(num)
-  end 
-  
-  def page_count 
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre 
-  end 
-  
-  def genre 
-    @genre 
   end 
   
   def turn_page
